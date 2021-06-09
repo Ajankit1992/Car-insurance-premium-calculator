0d85d8d1cad1be56f870b6e1bb303221de85e727
Ajankit1992 <38551942+Ajankit1992@users.noreply.github.com>
2021-06-09 07:25:12 +0530

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# A Car insurance premium calculator
+An easy to use and deploy on javascript and html.
+
+# Features:
+
+* Calculate car insurance premium with user's basic details.
+
+# Thanks
---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".total").text("Total: $0 /month");
+  });
+
+// $('input,select').on('change', refreshTotal);
+
+function refreshTotal() {
+    var gender = +$('input[name="gender"]:checked', '#gender').val();
+    var age = +$('input[name="age"]:checked', '#age').val();
+    var car = +$('#car').val();
+    var year = +$('#year').val();
+    
+    if (isNaN(gender)) gender = 0;
+    if (isNaN(age)) age = 0;
+    if (isNaN(car)) car = 0;
+    if (isNaN(year)) year = 0;
+    
+    var total = gender + age + car + year;
+    $(".total").text("Total: $" + total + " /month");
+}
